4ed98146be37c8edd8ce0a81cd03620efc54ebf5
karansaini0143 <karansainijk10@gmail.com>
2026-08-05 15:59:29 +0530

    Time: 4 ms (59.66%), Space: 16.5 MB (6.47%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        stack<long long> st;
+        stack<long long> sc;
+
+        int n=nums.size();
+        vector<long long> ngrt(n);
+        vector<long long> pgrt(n);
+        vector<long long> nlss(n);
+        vector<long long> plss(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                st.pop();
+            }
+            while(!sc.empty() && nums[sc.top()]>nums[i]){
+                sc.pop();
+            }
+            if(sc.empty()) plss[i]=-1;
+            else plss[i]=sc.top();
+            if(st.empty()) pgrt[i]=-1;
+            else pgrt[i]=st.top();
+
+            st.push(i);
+            sc.push(i);
+        }
+        while(!st.empty()) st.pop();
+        while(!sc.empty()) sc.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i]){
+                st.pop();
+            }
+            while(!sc.empty() && nums[sc.top()]>=nums[i]){
+                sc.pop();
+            }
+            if(sc.empty()) nlss[i]=n;
+            else nlss[i]=sc.top();
+            if(st.empty()) ngrt[i]=n;
+            else ngrt[i]=st.top();
+
+            st.push(i);
+            sc.push(i);
+        }
+        long long sum=0;
+        long long high;
+        long long low;
+        for(int i=0;i<n;i++){
+            high=nums[i]*(ngrt[i]-i)*(i-pgrt[i]);
+            low=nums[i]*(nlss[i]-i)*(i-plss[i]);
+            sum+=high-low;
+        }
+        return sum;
+    }
+};
